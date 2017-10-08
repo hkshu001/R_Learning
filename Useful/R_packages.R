4e9ff43eaa9672490afeb9d566372e18d4c23fa2
Hank Hsu <hkshu001@yahoo.com.tw>
2017-10-08 09:53:27 +0800

    add "tidyverse"

--- Useful/R_packages.R
+++ Useful/R_packages.R
@@ -1,14 +1,23 @@
-pkg_list <- c("ggplot2", "plyr", "dplyr", "reshape2", "Rcpp", "digest", "stringr",
-  "lubridate","knitr", "shiny", "RODBC", "RSQLite", "rmarkdown",
-  "broom","jpeg", "jsonlite", "PythonInR","Lahman", "microbenchmark",
-  "png", "pryr", "purrr", "rcorpora", "readr",  "tibble", "tidyr",
-  "qdap","magrittr","data.table","devtools","readxl","openxlsx","dtplyr","leaftlet", 
-  "ggplot2movies","ggthemes","ggthemr","extrafont", "cowplot", "ggrepel","plotly",
-  "prettydoc","colorspace","installr", "httr","jsonlite","rvest","ggforce","xml2",
-  "GGally","googleVis","ggmap")
+pkg_list <- c("reshape2", "Rcpp", "digest","shiny", "RODBC", "RSQLite","jpeg", "PythonInR","Lahman",
+              "microbenchmark","png", "pryr", "rcorpora","qdap","data.table","devtools","openxlsx","dtplyr","leaftlet", 
+              "ggplot2movies","ggthemes","ggthemr","extrafont", "cowplot", "ggrepel","plotly",
+              "prettydoc","colorspace","installr","ggforce","GGally","googleVis","ggmap","tidyverse")
 
 pkg_new <- pkg_list[!(pkg_list %in% installed.packages()[,"Package"])]
 if(length(pkg_new)) install.packages(pkg_new)
 rm(pkg_new, pkg_list)
 
 
+# "tidyverse"
+#========================================================================
+# Import: c("readr", "readxl", "haven", "httr", "rvest", "xml2")
+# Tidy: c("tibble", "tidyr")
+# Transform: c("dplyr", "forcats", "hms", "lubridate", "stringr")
+# Visualise: c("ggplot2")
+# Model: c("broom", "modelr")
+# # program: c("purrr", "magrittr")
+#  
+# 1.1.1
+# broom, dplyr, forcats, ggplot2, haven, httr, hms, jsonlite, lubridate, magrittr, 
+# modelr, purrr, readr, readxl, stringr, tibble, rvest, tidyr, xml2
+# knitr, rmarkdown
